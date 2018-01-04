618eababb90c97266779bef84008ad144c49e602
rygrma15 <rygrma15@DST20716>
2018-01-04 10:19:53 +0100

    2

--- Inventar/MainWindow.xaml.cs
+++ Inventar/MainWindow.xaml.cs
@@ -20,9 +20,35 @@ namespace Inventar
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool _isRectDragInProg;
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void rect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _isRectDragInProg = true;
+            rect.CaptureMouse();
+        }
+
+        private void rect_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _isRectDragInProg = false;
+            rect.ReleaseMouseCapture();
+        }
+
+        private void rect_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!_isRectDragInProg) return;
+
+            // get the position of the mouse relative to the Canvas
+            var mousePos = e.GetPosition(canvas);
+
+            // center the rect on the mouse
+            double left = mousePos.X - (rect.ActualWidth / 2);
+            double top = mousePos.Y - (rect.ActualHeight / 2);
+            Canvas.SetLeft(rect, left);
+            Canvas.SetTop(rect, top);
         }
     }
 }
