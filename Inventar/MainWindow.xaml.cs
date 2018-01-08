e36cf0612a046f9bd5fb3a550ce288caffa6f537
rygrma15 <rygrma15@rygrma15>
2018-01-08 22:34:03 +0100

    7

--- Inventar/MainWindow.xaml.cs
+++ Inventar/MainWindow.xaml.cs
@@ -32,92 +32,132 @@ namespace Inventar
 
         private void move_Rectangle(object sender, MouseButtonEventArgs e)
         {
-
-            List<int> rows = new List<int>();
-            List<int> columns = new List<int>();
+            int[,] pozice = new int[100,2];
+            /*while (true)
+            {*/
             if (_isRectDragInProg == false)
-            {
-                
-                obj = sender as Rectangle;
-
-                Console.WriteLine(obj.Name);
-                obj.CaptureMouse();
-                _isRectDragInProg = true;
-
-                foreach (UIElement cell in grid.Children)
-                {
-                    
-
-                    int x = Grid.GetColumn(cell);
-                    int y = Grid.GetRow(cell);
-                    int xa = Grid.GetColumnSpan(cell);
-                    int ya = Grid.GetRowSpan(cell);
-
-                    if (xa > 1)
-                    {
-                        columns.Add(x + 1);
-                        columns.Add(x);
-                    }
-                    else
-                    {
-                        columns.Add(x);
-                    }
-
-                    if (ya > 1)
-                    {
-                        rows.Add(y + 1);
-                        rows.Add(y);
-                    }
-                    else
-                    {
-                        rows.Add(y);
-                    }
-                }
-                int pocetradky = rows.Where(temp => temp.Equals(row))
-                        .Select(temp => temp)
-                        .Count();
-                int pocetsloupce = columns.Where(temp => temp.Equals(collum))
-                        .Select(temp => temp)
-                        .Count();
-
-                //Console.WriteLine(pocetradky);
-                //Console.WriteLine(pocetsloupce);
-                Console.WriteLine(pocetsloupce + pocetradky);
-                if ((pocetsloupce + pocetradky) < 4)
-                {
-                    obj.ReleaseMouseCapture();
-
-
-                    //_isRectDragInProg = false;
-                }
-                else
                 {
                     _isRectDragInProg = true;
+                    obj = sender as Rectangle;
+
+                    //Console.WriteLine(obj.Name);
+                    
+                    obj.CaptureMouse();
+                    
+
+
+
+                    //Console.WriteLine(pocetradky);
+                    //Console.WriteLine(pocetsloupce);
+
+
                 }
-                
+                else if (_isRectDragInProg == true)
+                {
+
+                    foreach (UIElement cell in grid.Children)
+                    {
+                        if (cell.GetValue(NameProperty).ToString() == obj.Name)
+                        {
+                            Console.WriteLine(cell.GetValue(NameProperty));
+                            Console.WriteLine(obj.Name);
+                        }
+                        else
+                        {
+                            Console.WriteLine(cell.GetValue(NameProperty));
+                        
+                            int x = Grid.GetColumn(cell);
+                            //Console.WriteLine("Pozice-sloupec:" + x);
+                            int y = Grid.GetRow(cell);
+                            //Console.WriteLine("Pozice-radek:" + y);
+                            int xa = Grid.GetColumnSpan(cell);
+                            //Console.WriteLine("Delka-sloupec:" + xa);
+                            int ya = Grid.GetRowSpan(cell);
+                            //Console.WriteLine("Delka-radek:" + ya);
+
+                            if (xa == 2)
+                            {
+                                columns.Add(x + 1);
+                                columns.Add(x);
+                            }
+                            else
+                            {
+                                columns.Add(x);
+                            }
+
+                            if (ya == 2)
+                            {
+                                rows.Add(y + 1);
+                                rows.Add(y);
+                            }
+                            else
+                            {
+                                rows.Add(y);
+                            }
+                            
+                        }
+                    
+                }
+                foreach (var o in rows)
+                {
+                    Console.WriteLine("radek: " + o);
+                }
+                foreach (var d in columns)
+                {
+                    Console.WriteLine("sloupec: " + d);
+                }
+
+                /*int pocetradky = rows.Where(temp => temp.Equals(row))
+                            .Select(temp => temp)
+                            .Count();
+                int pocetradka = rows.Where(temp => temp.Equals(row+1))
+                            .Select(temp => temp)
+                            .Count();
+                int pocetsloupce = columns.Where(temp => temp.Equals(collum))
+                            .Select(temp => temp)
+                            .Count();
+                int pocetsloupcea = columns.Where(temp => temp.Equals(collum+1))
+                            .Select(temp => temp)
+                            .Count();
+                //Console.WriteLine(pocetsloupce + pocetradky + pocetsloupcea + pocetradka);
+                    if ((pocetsloupce + pocetradky+ pocetsloupcea + pocetradka) < 5)
+                    {
+
+
+                        obj.ReleaseMouseCapture();
+                        _isRectDragInProg = false;
+                        //break;
+                    }
+                    else
+                    {
+                        _isRectDragInProg = true;
+                    }*/
+
+            }
+            //}
+
+
+
+        }
+
+
+            /*private void rect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+            {
+                _isRectDragInProg = true;
+                obj = sender as Rectangle;
+                obj.CaptureMouse();
+
             }
 
+            private void rect_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+            {
+                _isRectDragInProg = false;
+                obj.ReleaseMouseCapture();
+            }*/
 
-        }
-
-
-        /*private void rect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+            private void rect_MouseMove(object sender, MouseEventArgs e)
         {
-            _isRectDragInProg = true;
-            obj = sender as Rectangle;
-            obj.CaptureMouse();
-            
-        }
-
-        private void rect_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            _isRectDragInProg = false;
-            obj.ReleaseMouseCapture();
-        }*/
-
-        private void rect_MouseMove(object sender, MouseEventArgs e)
-        {
-            Console.WriteLine(_isRectDragInProg);
+            //Console.WriteLine(_isRectDragInProg);
             if (_isRectDragInProg == true)
             {
                 var mousePos = e.GetPosition(grid);
