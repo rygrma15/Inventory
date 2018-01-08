64d18be1cdceb553096e73bca2028bf5f9bcc72e
rygrma15 <rygrma15@rygrma15>
2018-01-08 17:03:56 +0100

    7

--- Inventar/MainWindow.xaml.cs
+++ Inventar/MainWindow.xaml.cs
@@ -22,23 +22,20 @@ namespace Inventar
     {
         Rectangle obj;
         private bool _isRectDragInProg = false;
+        int row;
+        int collum;
         public MainWindow()
         {
             InitializeComponent();
-            Console.WriteLine(Grid.ColumnProperty);
+            
         }
 
         private void move_Rectangle(object sender, MouseButtonEventArgs e)
         {
-            
-            
-            if (_isRectDragInProg == true)
-            {
-                obj.ReleaseMouseCapture();
 
-
-                _isRectDragInProg = false;
-            } else if (_isRectDragInProg == false)
+            List<int> rows = new List<int>();
+            List<int> columns = new List<int>();
+            if (_isRectDragInProg == false)
             {
                 
                 obj = sender as Rectangle;
@@ -46,6 +43,58 @@ namespace Inventar
                 Console.WriteLine(obj.Name);
                 obj.CaptureMouse();
                 _isRectDragInProg = true;
+
+                foreach (UIElement cell in grid.Children)
+                {
+                    
+
+                    int x = Grid.GetColumn(cell);
+                    int y = Grid.GetRow(cell);
+                    int xa = Grid.GetColumnSpan(cell);
+                    int ya = Grid.GetRowSpan(cell);
+
+                    if (xa > 1)
+                    {
+                        columns.Add(x + 1);
+                        columns.Add(x);
+                    }
+                    else
+                    {
+                        columns.Add(x);
+                    }
+
+                    if (ya > 1)
+                    {
+                        rows.Add(y + 1);
+                        rows.Add(y);
+                    }
+                    else
+                    {
+                        rows.Add(y);
+                    }
+                }
+                int pocetradky = rows.Where(temp => temp.Equals(row))
+                        .Select(temp => temp)
+                        .Count();
+                int pocetsloupce = columns.Where(temp => temp.Equals(collum))
+                        .Select(temp => temp)
+                        .Count();
+
+                //Console.WriteLine(pocetradky);
+                //Console.WriteLine(pocetsloupce);
+                Console.WriteLine(pocetsloupce + pocetradky);
+                if ((pocetsloupce + pocetradky) < 4)
+                {
+                    obj.ReleaseMouseCapture();
+
+
+                    //_isRectDragInProg = false;
+                }
+                else
+                {
+                    _isRectDragInProg = true;
+                }
+                
             }
 
 
@@ -68,50 +117,55 @@ namespace Inventar
 
         private void rect_MouseMove(object sender, MouseEventArgs e)
         {
-            if (!_isRectDragInProg) return;
-            // get the position of the mouse relative to the Canvas
-            var mousePos = e.GetPosition(grid);
-            double left;
-            double top;
-            // center the rect on the mouse
-            if (mousePos.X < 0)
+            Console.WriteLine(_isRectDragInProg);
+            if (_isRectDragInProg == true)
             {
-                left = 0;
-            }
-            else if (mousePos.X > 450)
-            {
-                left = 8; 
-            }
-            else
-            {
-                left = mousePos.X / 50;
-            }
+                var mousePos = e.GetPosition(grid);
+                double left;
+                double top;
+                if (mousePos.X < 0)
+                {
+                    left = 0;
+                }
+                else if (mousePos.X > 450)
+                {
+                    left = 8;
+                }
+                else
+                {
+                    left = mousePos.X / 50;
+                }
 
-            if (mousePos.Y < 0)
-            {
-                top = 0;
-            }
-            else if (mousePos.Y > 250)
-            {
-                top = 4;
-            }
-            else
-            {
-                top = mousePos.Y / 50;
-            } 
+                if (mousePos.Y < 0)
+                {
+                    top = 0;
+                }
+                else if (mousePos.Y > 250)
+                {
+                    top = 4;
+                }
+                else
+                {
+                    top = mousePos.Y / 50;
+                }
 
-            int collum = (int)left;
-            if (Grid.GetColumnSpan(obj) == 2 && collum == 8)
-            {
-                collum = collum - 1;
+                collum = (int)left;
+                if (Grid.GetColumnSpan(obj) == 2 && collum == 8)
+                {
+                    collum = collum - 1;
+                }
+                row = (int)top;
+                if (Grid.GetRowSpan(obj) == 2 && row == 4)
+                {
+                    row = row - 1;
+                }
+
+
+                Grid.SetColumn(obj, collum);
+                Grid.SetRow(obj, row);
             }
-            int row = (int)top;
-            if (Grid.GetRowSpan(obj) == 2 && row == 4)
-            {
-                row = row - 1;
-            }
-            Grid.SetColumn(obj, collum);
-            Grid.SetRow(obj, row);
+            
+
 
              
 
